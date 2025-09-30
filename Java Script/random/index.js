1bc85ba3531e973d1393e8608a5165f4b20fc59a
Yashwanthvm06 <yashwanthvm06@gmail.com>
2025-09-30 18:15:12 +0530

    random number dice added

--- Java Script/random/index.js
+++ Java Script/random/index.js
@@ -1,7 +1,17 @@
-
-const min=50;
-const max=100;
-
-let rabdomnum=Math.floor((Math.random() * (max-min) )+min);
-
-console.log(rabdomnum);
+const btn=document.getElementById("btn");
+const dice1=document.getElementById("dice1");
+const dice2=document.getElementById("dice2");
+const dice3=document.getElementById("dice3");
+const min=1;
+const max=6;
+ let rn1;
+ let rn2;
+ let rn3;
+btn.onclick=function(){
+    rn1=Math.floor(Math.random()*(max-min)+min);
+    rn2=Math.floor(Math.random()*(max-min)+min);
+    rn3=Math.floor(Math.random()*(max-min)+min);
+    dice1.textContent=rn1;
+    dice2.textContent=rn2;
+    dice3.textContent=rn3;
+}
