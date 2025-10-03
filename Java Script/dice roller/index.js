388ec9deafddb010a12acfd1bd27d7eaa8ea506d
Yashwanthvm06 <yashwanthvm06@gmail.com>
2025-10-03 22:36:35 +0530

    modified dice roller file

--- Java Script/dice roller/index.js
+++ Java Script/dice roller/index.js
@@ -8,9 +8,12 @@ function roll(){
 
     for(let i=0;i<noofdice;i++){
         const value=Math.floor(Math.random()*6)+1;
-        values.push(value);
-        images.push(`<img src="img/${value}.jpg" style="width:80px;height:80px;margin:5px;">`)    
+        values.push(value);images.push(`<div style="display:inline-block; text-align:center; margin:5px;">
+                <img src="img/${value}.jpg" style="width:80px; height:80px;">
+                <div style="font-size:20px; font-weight:normal;">dice no ${i+1}</div>
+            </div>`);
+   
     }
-    diceresult.textContent=`dice:${values.join(", ")}`;
+    diceresult.textContent=`dice values :${values.join(", ")}`;
     diceimages.innerHTML=images.join(' ');
 }
