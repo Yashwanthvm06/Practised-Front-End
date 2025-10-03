71535b1cf338a2c6cd7963873dfa7f37c095950e
Yashwanthvm06 <yashwanthvm06@gmail.com>
2025-10-03 16:06:43 +0530

    modified the logical operator

---
+++ Java Script/logical operator/index.js
@@ -0,0 +1,19 @@
+/*
+const temp=200;
+
+if(temp <= 0 || temp > 30){
+console.log("the weather is BAD");
+}
+else{
+    console.log("the weather is GOOD");
+}
+    */
+
+const ishot=true;
+
+if(ishot){
+    console.log("It is SUNNY");
+}
+else{
+    console.log("It is CLOUDY");
+}
