01b0f9a30b3acb5696464afc80c892968e259b35
Yashwanthvm06 <yashwanthvm06@gmail.com>
2025-10-03 16:12:57 +0530

    strict equality file added

---
+++ Java Script/strict equalit/index.js
@@ -0,0 +1,20 @@
+/*
+const PI=3.1415;
+if(PI==="3.1415"){
+    console.log("that is pi");
+}
+else{
+    console.log("this is not pi");
+}
+    == check the value regardless of data type
+    === check including the data type
+*/
+
+const PI=3.14;
+
+if(PI !== "3.14"){
+    console.log("That is NOT PI");
+}
+else{
+    console.log("That is PI");
+}
