3a58facd4802ecc4d6e6027ff0f7cc8f36081888
Yashwanthvm06 <yashwanthvm06@gmail.com>
2025-10-03 13:17:33 +0530

    String files are added

---
+++ Java Script/string/index.js
@@ -0,0 +1,43 @@
+let username="yashwanth";
+
+//console.log(username.charAt(1));
+
+//console.log(username.indexOf(1));
+
+//console.log(username.lastIndexOf("a"));
+
+//console.log(username.length);
+
+
+/*let username1="yashwanth        ";
+console.log(username1);
+
+username1=username1.trim();
+
+console.log(username1);*/
+
+//console.log(username.toUpperCase());
+//console.log(username.toLowerCase());
+
+//console.log(username.repeat(4));
+
+
+/*
+let username2="0yaya "
+
+let result=username2.startsWith("0") || username2.endsWith(" ");
+
+if(result){
+    console.log(" your username can't consist of digit or 0 at start and ' '  at end ");   
+}
+
+else{
+    console.log("your username is good to go");
+}
+*/
+
+let phonenumber ="91-6381-269-700";
+
+phonenumber=phonenumber.padEnd(20,"y");
+console.log(phonenumber);
+//console.log(phonenumber.replaceAll("-",""));
